3c5f0c3f16b0836d002870f581e0b4d8898977ad
crazywolf132 <crazywolf132@gmail.com>
2019-02-27 18:56:02 +1100

    Updated file to contain new methods.

--- src/main/java/com/sol/ui/SolUI.java
+++ src/main/java/com/sol/ui/SolUI.java
@@ -1,7 +1,10 @@
 package com.sol.ui;
 
 import com.sol.Sol;
+import com.sol.util.CloseHandler;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -19,5 +22,22 @@ public class SolUI {
         Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
 
         Stage stage = sol.getStage();
+        stage.setWidth(bounds.getWidth() - 2);
+        stage.setHeight(bounds.getHeight() * 0.9);
+        stage.setX((bounds.getWidth() - stage.getWidth()) / 2);
+        stage.setY((bounds.getHeight() - stage.getHeight()) / 2);
+
+        Parent root = layout.getBorderPane();
+        //TODO: Create style sheets...
+        //root.getStylesheets().add();
+
+        Scene scene = new Scene(root, stage.getWidth(), stage.getHeight());
+        stage.setScene(scene);
+
+        CloseHandler handler = new CloseHandler(sol);
     }
+
+    public SolLayout getLayout() { return layout; }
+    
+    public Sol getSol() { return sol; }
 }
