00c6ec3f9390f27bf366ba4fdba21168a95fb0ec
crazywolf132 <crazywolf132@gmail.com>
2019-02-27 18:13:18 +1100

    Created first Files.

---
+++ src/main/java/com/mani/ide/ManiIDE.java
@@ -0,0 +1,27 @@
+package com.mani.ide;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ManiIDE extends Application{
+
+    public static ManiIDE instance;
+
+    private Stage stage;
+    private ManiUI ui;
+
+    public ManiIDE() {
+        instance = this;
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.stage = stage;
+        this.ui = new ManiUI(this);
+
+        ui.initialize();
+
+        stage.show();
+    }
+}
---
+++ src/main/java/com/mani/ide/ManiLauncher.java
@@ -0,0 +1,10 @@
+package com.mani.ide;
+
+import javafx.application.Application;
+
+public class ManiLauncher {
+
+    public static void main(String[] args) {
+         Application.launch(ManiIDE.class, args);
+    }
+}
