4f358b84745a583d3bef376afb71adcab5acf828
crazywolf132 <crazywolf132@gmail.com>
2019-02-27 18:59:34 +1100

    Created constants handler class.
    This is the class that keeps the project name and its current version.

---
+++ src/main/java/com/sol/SolConts.java
@@ -0,0 +1,15 @@
+package com.sol;
+
+public class SolConts {
+
+    public static final String NAME = "Sól";
+
+    /**
+     * How the version system works... It is actually very simple.
+     * YEAR.MONTH NUMBER.AMOUNT OF RELEASES THIS MONTH
+     *
+     * As you can see, very simple and effective.
+     */
+    public static final String VERSION = "2019.02.01";
+
+}
