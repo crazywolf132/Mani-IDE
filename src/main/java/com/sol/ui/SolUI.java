786c860c6f49062cf99510a31c438a0fcd704081
crazywolf132 <crazywolf132@gmail.com>
2019-02-27 18:55:35 +1100

    Created frame.

--- src/main/java/com/sol/ui/SolUI.java
+++ src/main/java/com/sol/ui/SolUI.java
@@ -1,6 +1,9 @@
 package com.sol.ui;
 
 import com.sol.Sol;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 
 public class SolUI {
 
@@ -9,6 +12,12 @@ public class SolUI {
 
     public SolUI(Sol sol) {
         this.sol = sol;
+        this.layout = new SolLayout("ui");
+    }
 
+    public void initialize() {
+        Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+
+        Stage stage = sol.getStage();
     }
 }
