e9d4aa4bf330ae4527da0024be6709ef86d3fa6a
crazywolf132 <crazywolf132@gmail.com>
2019-02-27 19:33:08 +1100

    Added PluginFinder to startup

--- src/main/java/com/sol/Sol.java
+++ src/main/java/com/sol/Sol.java
@@ -1,5 +1,7 @@
 package com.sol;
 
+import com.sol.plugin.PluginFinder;
+import com.sol.plugin.PluginManager;
 import com.sol.ui.SolUI;
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -9,11 +11,14 @@ public class Sol extends Application{
 
     public static Sol instance;
 
+    private final PluginManager pluginManager;
     private Stage stage;
     private SolUI ui;
 
     public Sol() {
         instance = this;
+
+        this.pluginManager = new PluginManager(this);
 
     }
 
@@ -22,12 +27,19 @@ public class Sol extends Application{
         this.stage = stage;
         this.ui = new SolUI(this);
 
+        PluginFinder pluginFinder = new PluginFinder(pluginManager);
+        pluginFinder.find();
+
         ui.initialize();
+        pluginManager.loadPlugins();
+        pluginManager.enablePlugins();
 
         stage.show();
     }
 
     public void exit() {
+        pluginManager.disablePlugins();
+
         Platform.exit();
         System.exit(-1);
     }
