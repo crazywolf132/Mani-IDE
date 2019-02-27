0c61cc0af3c44e810547191ee81636a956eea210
crazywolf132 <crazywolf132@gmail.com>
2019-02-27 18:13:30 +1100

    Added ManiUI

---
+++ src/main/java/com/mani/ide/ui/ManiUI.java
@@ -0,0 +1,14 @@
+package com.mani.ide.ui;
+
+import com.mani.ide.ManiIDE;
+
+public class ManiUI {
+
+    private final ManiIDE maniIDE;
+    private ManiLayout layout;
+
+    public ManiUI(ManiIDE maniIDE) {
+        this.maniIDE = maniIDE;
+
+    }
+}
