53a57dd969074f5100e16501ba7fd51b33e5d5a4
crazywolf132 <crazywolf132@gmail.com>
2019-02-27 18:13:24 +1100

    Added ManiLayout

---
+++ src/main/java/com/mani/ide/ui/ManiLayout.java
@@ -0,0 +1,42 @@
+package com.mani.ide.ui;
+
+import javafx.scene.layout.BorderPane;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ManiLayout {
+
+    private final String name;
+    private final Collection<ManiLayout> layouts;
+    private final BorderPane borderPane;
+
+    public ManiLayout(String name) { this(name, new BorderPane()); }
+
+    public ManiLayout(String name, BorderPane borderPane) {
+        this.name = name;
+        this.borderPane = borderPane;
+        this.layouts = new ArrayList<>();
+    }
+
+    public ManiLayout findLayout(String name) {
+        if (getName().equals(name)) {
+            return this;
+        }
+
+        for (ManiLayout layout: layouts) {
+            String layoutName = layout.getName();
+
+            if (layoutName.equals(name)) {
+                return layout;
+            }
+        }
+
+        return null;
+    }
+
+    public void addLayout(ManiLayout layout) { layouts.add(layout); }
+    public BorderPane getBorderPane() { return borderPane; }
+    public Collection<ManiLayout> getLayouts() { return layouts; }
+    public String getName() { return name; }
+}
